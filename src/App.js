7a39eb189b5b13ab55d35211c9ef6d6e65b591d5
Akif Eren Ertuğrul <akiferenertugrul@gmail.com>
2025-08-11 19:26:41 +0300

    List and To-Do functionality and UI/UX update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import List from "./components/List";
+import Todos from "./components/Todos";
+
+import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [selectedList, setSelectedList] = useState(null);
+
+    return (
+        <Container fluid className="p-3" style={{ height: "100vh" }}>
+            <Row className="h-100">
+                <Col md={4} className="border-end overflow-auto">
+                    <List
+                        selectedList={selectedList}
+                        onSelectList={setSelectedList}
+                    />
+                </Col>
+                <Col md={8} className="overflow-auto">
+                    <Todos selectedList={selectedList} />
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default App;
